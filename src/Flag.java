9f6e3822a35ac85bdd4d1a8a20ad909514f0a71f
Pascal Alfadian Nugroho <pascal@unpar.ac.id>
2017-08-16 10:22:22 +0700

    Initial project files and Flag code

---
+++ src/Flag.java
@@ -0,0 +1,23 @@
+/**
+ * Licensed under GPL (available in source code directory)
+ * @author pascal
+ */
+public class Flag {
+    public static final int HEIGHT = 10, WIDTH = 30;
+    public static final String MESSAGE = "72 tahun Indonesia Kerja Bersama / Teknik Informatika UNPAR /";
+    public static void main(String[] args) {
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                if (i == 0 || j == 0 || i == HEIGHT - 1 || j == WIDTH - 1) {
+                    System.out.print("\033[0m");
+                } else if (i < HEIGHT / 2) {
+                    System.out.print("\033[37;41m");
+                } else {
+                    System.out.print("\033[31;47m");
+                }
+                System.out.print(MESSAGE.charAt((i * WIDTH + j) % MESSAGE.length()));
+            }
+            System.out.println();
+        }
+    }
+}
